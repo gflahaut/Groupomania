a171e2736d0ce8d79dc3a4e769dadc47ea8b5bf0
gflahaut <75680769+gflahaut@users.noreply.github.com>
2021-08-04 22:57:33 +0100

    Add files via upload

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,21 +1,19 @@
 const mysql = require("../config/db.config");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-// const jwtConfig = require("../config/jwt");
-
-
 
 exports.createUser = async function createUser(newUser) {
   try {
+    console.log("user.model", newUser);
     newUser.password = await bcrypt.hash(newUser.password, 10);
     const query = mysql.request(
       "INSERT INTO `users` (`username`, `password`, `name`, `firstname`, `email`) VALUES (?, ? , ? , ?, ? );",
       [
-        newUser.firstname.charAt(0)+newUser.name,
+        newUser.username,
         newUser.password,
         newUser.name,
         newUser.firstname,
-        newUser.email = firstname.charAt(0)+newUser.name+'@groupomania.com',
+        newUser.email,
       ]
     );
   } catch (error) {
@@ -26,15 +24,20 @@ exports.createUser = async function createUser(newUser) {
 
 exports.compareUser = async function compareUser(user) {
   try {
-    const token = jwt.sign({
-      data: user.id,
-    }, process.env.JWT_SECRET, { expiresIn: '5h' }); //5 hour
-    console.log('user.model.compareUser',token);
+    
+    const token = jwt.sign(
+      {
+        data: user.id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "5h" }
+    ); 
     const query = await mysql.request(
       "SELECT * FROM Users WHERE username ='" + user.username + "';"
     );
-    if (query.length == 0){
-      return ({ code : 1, message :" Username not found !"});
+    console.log(query);
+    if (query.length == 0) {
+      return { code: 1, message: " Username not found !" };
     }
     let passwordChecked = await bcrypt.compare(
       user.password,
@@ -43,11 +46,12 @@ exports.compareUser = async function compareUser(user) {
     if (passwordChecked) {
       return {
         userid: query[0].userId,
-        token : token,
-        username: query[0].username
+        token: token,
+        username: query[0].username,
+        role: query[0].role,
       };
-    }else{
-      return ({ code : 2, message :" Wrong Password ! "});
+    } else {
+      return { code: 2, message: " Wrong Password ! " };
     }
   } catch (error) {
     console.log(error);
@@ -55,17 +59,16 @@ exports.compareUser = async function compareUser(user) {
   }
 };
 
-
 exports.getInfos = async function getInfos(userid) {
   console.log(userid);
   try {
-    const query = await mysql.request(
-      "SELECT * FROM users WHERE userid = ?;", [userid]
-    );
-    if (query.length > 0){
-      return (query);
-    }else{
-      return ({ code : 1, message : " Found no users matching your search !"})
+    const query = await mysql.request("SELECT * FROM users WHERE userid = ?;", [
+      userid,
+    ]);
+    if (query.length > 0) {
+      return query;
+    } else {
+      return { code: 1, message: " Found no users matching your search !" };
     }
   } catch (error) {
     console.log(error);
@@ -77,12 +80,13 @@ exports.getRole = async function getRole(userid) {
   console.log(userid);
   try {
     const query = await mysql.request(
-      "SELECT role FROM users WHERE userid = ?;", [userid]
+      "SELECT role FROM users WHERE userid = ?;",
+      [userid]
     );
-    if (query.length > 0){
-      return (query);
-    }else{
-      return ({ code : 1, message : " Found no user matching this id !"})
+    if (query.length > 0) {
+      return query;
+    } else {
+      return { code: 1, message: " Found no user matching this id !" };
     }
   } catch (error) {
     console.log(error);
@@ -91,25 +95,31 @@ exports.getRole = async function getRole(userid) {
 };
 
 exports.getUsersFromSearch = async function getUsersFromSearch(search) {
-  // console.log(search);
   try {
     let query = "";
-    if(search === search.split(" ")[0]){
-       query = await mysql.request(
-        "SELECT * FROM users WHERE username LIKE '"+search+"%' OR name LIKE '"+search+"%' OR firstname LIKE '"+search+"%';"
-      );
-    }else{
-      search = search.split(" ");
-      // console.log("HERE 2");
-      // console.log(search[0]);
-      // console.log(search[1]);
+    if (search === search.split(" ")[0]) {
       query = await mysql.request(
-        "SELECT * FROM users WHERE firstname LIKE '"+search[0]+"%' && name LIKE '"+search[1]+"%';"
+        "SELECT * FROM users WHERE username LIKE '" +
+          search +
+          "%' OR name LIKE '" +
+          search +
+          "%' OR firstname LIKE '" +
+          search +
+          "%';"
+      );
+    } else {
+      search = search.split(" ");
+      query = await mysql.request(
+        "SELECT * FROM users WHERE firstname LIKE '" +
+          search[0] +
+          "%' && name LIKE '" +
+          search[1] +
+          "%';"
       );
     }
-    console.log(query)
-    if (query.length > 0){
-      return (query);
+    console.log(query);
+    if (query.length > 0) {
+      return query;
     }
   } catch (error) {
     console.log(error);
@@ -117,14 +127,14 @@ exports.getUsersFromSearch = async function getUsersFromSearch(search) {
   }
 };
 
-exports.deleteUser = async function deleteUser(userid) {
-  console.log(userid);
-  try{
-    const query = await mysql.request(
-      "DELETE FROM users WHERE userid = ? ;",[userid]
-    );
+exports.deleteUser = async function deleteUser(user) {
+  try {
+    const query = mysql.request("DELETE FROM users WHERE userid = ?", [
+      user,
+    ]);
     return query;
-  }catch(error){
-    throw error;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 };
